818575c893e1697730e53cb4e2bf654f01bf72e7
elsayed85 <elsayed85@users.noreply.github.com>
2023-08-25 07:00:32 +0000

    Fix styling

--- src/Fawry.php
+++ src/Fawry.php
@@ -289,6 +289,7 @@ class Fawry implements RequiredFields
     private function generate_html($data): string
     {
         $viewData = ['model' => $this, 'data' => $data];
+
         return view('fawry::form', $viewData)->render();
     }
 }
